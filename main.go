6e6e6e418f2b01142e964937e34891e3860efec9
Pieter Claerhout <pieter@yellowduck.be>
2020-12-16 08:41:12 +0100

    Added the GOARCH and fixed a bug in the error checking

--- main.go
+++ main.go
@@ -2,22 +2,23 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"syscall"
 )
 
 func main() {
 
 	r, err := syscall.Sysctl("sysctl.proc_translated")
-	if err.Error() == "no such file or directory" {
-		fmt.Println("Running on intel mac")
+	if err != nil && err.Error() == "no such file or directory" {
+		fmt.Println("Running on intel mac, arch:", runtime.GOARCH)
 	}
 
 	if r == "\x00\x00\x00" {
-		fmt.Println("Running on apple silicon natively")
+		fmt.Println("Running on apple silicon natively, arch:", runtime.GOARCH)
 	}
 
 	if r == "\x01\x00\x00" {
-		fmt.Println("Running on apple silicon under Rosetta 2")
+		fmt.Println("Running on apple silicon under Rosetta 2, arch:", runtime.GOARCH)
 	}
 
 }
